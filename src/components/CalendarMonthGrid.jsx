146c3a5f8fc5ba4a6d92f0681e36862b94986019
fcarrella <francesco.carrella@gmail.com>
2018-01-25 17:01:24 +0100

    Use separate PropType package (instead of React built-in)

--- src/components/CalendarMonthGrid.jsx
+++ src/components/CalendarMonthGrid.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import shallowCompare from 'react-addons-shallow-compare';
 import momentPropTypes from 'react-moment-proptypes';
